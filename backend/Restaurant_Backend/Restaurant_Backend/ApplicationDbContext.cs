df119bdf3704c3c99b738f695c368c20cf2db80c
gg300 <georgebot300@yahoo.com>
2025-12-01 01:01:32 +0200

    first demo

---
+++ backend/Restaurant_Backend/Restaurant_Backend/ApplicationDbContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Restaurant_Backend.Entities;
+namespace Restaurant_Backend;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+{
+    public DbSet<Category> Categories{ get; set; }
+}
--- backend/Restaurant_Backend/Restaurant_Backend/Repository/Category/ICategoryRepository.cs
+++ backend/Restaurant_Backend/Restaurant_Backend/Repository/Category/ICategoryRepository.cs
@@ -1,6 +1,12 @@
-﻿namespace Restaurant_Backend.Repository.Category;
+﻿using Restaurant_Backend.Entities;
 
-public class ICategoryRepository
+namespace Restaurant_Backend.Repository.Category;
+
+public interface ICategoryRepository
 {
-    
+    Entities.Category? GetById(int id);
+    List<Entities.Category> GetAll();
+    void Add(Entities.Category category);
+    void Update(Entities.Category category);
+    void Delete(Entities.Category category);
 }
