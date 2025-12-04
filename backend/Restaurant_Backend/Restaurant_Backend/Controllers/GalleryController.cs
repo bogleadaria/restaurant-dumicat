2d9b2e2f02d2bee804d3ad841c088c9c70651d46
gg300 <georgebot300@yahoo.com>
2025-12-04 17:02:21 +0200

    hih

--- backend/Restaurant_Backend/Restaurant_Backend/Controllers/GalleryController.cs
+++ backend/Restaurant_Backend/Restaurant_Backend/Controllers/GalleryController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Restaurant_Backend.Repository;
 using Restaurant_Backend.Entities;
+using Restaurant_Backend.Repository.Gallery;
 
 namespace Restaurant_Backend.Controllers;
 
@@ -8,5 +9,49 @@ namespace Restaurant_Backend.Controllers;
 [Route("api/gallery")]
 public class GalleryController : ControllerBase
 {
-    
+    private readonly IGalleryRepository _galleryRepository;
+    public GalleryController(IGalleryRepository galleryRepository)
+    {
+        _galleryRepository = galleryRepository;
+    }
+
+    [HttpGet]
+    public IActionResult GetGalleries()
+    {
+        return  Ok(_galleryRepository.GetAll());
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetGallery(int id)
+    {
+        return Ok(_galleryRepository.GetById(id));
+    }
+
+    [HttpPost]
+    public IActionResult AddGallery(Gallery gallery)
+    {
+        var tmp_gallery = _galleryRepository.GetById(gallery.Id);
+        if (tmp_gallery != null)
+        {
+            return BadRequest("Photo already exists");
+        }
+
+        _galleryRepository.Add(gallery);
+        return Ok(gallery);
+    }
+
+    [HttpPut]
+    public IActionResult UpdateGallery(Gallery gallery)
+    {
+        _galleryRepository.Update(gallery);
+        return Ok(gallery);
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult DeleteGallery(int id)
+    {
+        var gallery = _galleryRepository.GetById(id);
+        _galleryRepository.Delete(gallery);
+        return Ok();
+    }
 }
